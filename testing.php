ec051e66ad9f615b84e98c85becc670feafdb2f2
Kellan Stevens <kellan@kellanstevens.com>
2024-01-27 15:48:43 +0200

    Fixed SVG file and README.md
    Realised I misspelled app-logo.svg as app-logo.vsg

---
+++ testing.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SVG Viewer</title>
+</head>
+<body>
+    <img src="app-logo.svg" alt="SVG">
+    <img src="/Users/kellanstevens/Herd/daniel-kellan/public/icons/icon.svg" alt="SVG">
+</body>
+</html>

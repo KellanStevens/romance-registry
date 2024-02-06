2c430c54b2507542ceb096658414c6f6bb1f9dbe
Kellan Stevens <kellan@kellanstevens.com>
2024-02-06 16:05:55 +0200

    Updating DateNight Model to support relationships correctly

--- app/Models/DateNight.php
+++ app/Models/DateNight.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DateNight extends Model
 {
@@ -11,12 +12,12 @@ class DateNight extends Model
 
     protected $fillable = ['date', 'location', 'google_maps_link', 'description'];
 
-    public function expense()
+    public function expenses(): HasMany
     {
         return $this->hasMany(Expense::class);
     }
 
-    public function rating()
+    public function ratings(): HasMany
     {
         return $this->hasMany(Rating::class);
     }

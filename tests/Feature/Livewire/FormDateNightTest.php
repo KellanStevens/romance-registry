a26d4f934d7ff6e96f7421a13912a9ab2e009209
Kellan Stevens <kellan@kellanstevens.com>
2024-02-10 15:57:40 +0200

    test: started with FormDateNight tests

---
+++ tests/Feature/Livewire/FormDateNightTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Livewire\FormDateNight;
+use App\Models\DateNight;
+use Livewire\Livewire;
+
+it('renders successfully', function () {
+    Livewire::test(FormDateNight::class)
+        ->assertStatus(200);
+});
+
+it('renders the correct title when editing a date night', function () {
+    $dateNight = DateNight::factory()->create();
+
+    Livewire::test(FormDateNight::class, ['dateNightId' => $dateNight->id])
+        ->assertSee('Edit Date Night');
+
+    $dateNight->delete();
+});
+
+it('renders the correct title when creating a date night', function () {
+    Livewire::test(FormDateNight::class)
+        ->assertSee('Create Date Night');
+});
+
+it('validates required fields', function () {
+    Livewire::test(FormDateNight::class)
+        ->call('submit')
+        ->assertHasErrors(['date' => 'required', 'location' => 'required']);
+});
